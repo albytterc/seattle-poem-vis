6eac16231327590e0001405afdb450202a03f872
Albert Terc <albytterc@protonmail.com>
2023-12-01 13:27:52 -0500

    Stop btn animation if clicked. Adjust btns' z-idx

--- src/components/VisWrapper.tsx
+++ src/components/VisWrapper.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren, useContext } from "react";
+import { PropsWithChildren, useContext, useState } from "react";
 import { PageContext } from "../PageContext";
 import PoemViewer from "./PoemViewer";
 import Stepper from "./Stepper";
@@ -14,24 +14,27 @@ function VisWrapper({
   children,
 }: PropsWithChildren<VisWrapperProps>) {
   const { pageIndex, setPageIndex } = useContext(PageContext);
+  const [helpClicked, setHelpClicked] = useState(false);
+
   return (
     <div className="h-screen w-screen bg-black/[.85] relative">
       {showPoemViewer && <PoemViewer />}
       <div className={`absolute right-0 flex flex-row gap-1`}>
         <button
-          className={`bg-[#D9D9D9] p-2 rounded ${
-            pageIndex === 1 ? "animate-pulse" : ""
+          className={`bg-[#D9D9D9] p-2 rounded z-50 ${
+            pageIndex === 1 && !helpClicked ? "animate-pulse" : ""
           }`}
-          onClick={() =>
+          onClick={() => {
+            setHelpClicked(true);
             alert(
-              "The poem loads one line at a time underneath the title. Use the arrow keys or the buttons at the bottom to navigate.\n\nPlay around with the charts and poem to interact with them!"
-            )
-          }
+              "The poem loads one line at a time underneath the title. Hover over the poem line to view the entire verse, click on the expand arrow to view the entire poem.\n\nUse the arrow keys or the buttons at the bottom to navigate.\n\nPlay around with the charts to interact with them!"
+            );
+          }}
         >
           Help
         </button>
         <button
-          className={`bg-[#D9D9D9] p-2 rounded ${
+          className={`bg-[#D9D9D9] p-2 rounded z-50 ${
             pageIndex === 17 ? "animate-pulse" : ""
           }`}
           onClick={() => window.location.reload()}
